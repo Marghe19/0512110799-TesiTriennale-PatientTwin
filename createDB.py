bd00aadc6c1ebc2fcae75ce651f5bd4aedfc4de5
Marghe19 <104621888+Marghe19@users.noreply.github.com>
2023-05-21 19:16:44 +0200

    Aggiunta file requirements; creato e popolato database

--- createDB.py
+++ createDB.py
@@ -37,19 +37,4 @@ cursor.execute("CREATE TABLE IF NOT EXISTS storico (id_utente INT,"
                "date DATE NOT NULL)")
 
 
-sql_patient = "INSERT INTO patient (name, surname, CF, birth, cellular) VALUES (%s,%s,%s,%s,%s)"
-values_patient =[ ("Mario", "Rossi", "DGBFHA59C30A976M", '1950-5-15', "3334050124"),
-          ("Alessandra", "Napolitano", "LVMMRW83D16I718T", '1970-11-23', "3809988012")
-          ]
 
-sql_user = "INSERT INTO user (name, surname, username, password, email, sector, role) VALUES (%s,%s,%s,%s,%s,%s,%s)"
-values_user = [ ("Francesco", "Alfieri", "f.alfieri", "passalfieri", "f.alfieri@medici.com", "cardiochirurgia", "medico"),
-                ("Anna", "Scotti", "a.scotti", "passscotti", "a.scotti@admin.com", "amministrazione", "admin")
-
-]
-
-
-cursor.executemany(sql, values)
-
-
-mydb.commit()
